67b74f23ac7068a4aaeb7be8f0a2fefe6b50f1f9
RailonA <railonacosta@gmail.com>
2021-09-27 17:59:41 -0400

    Add styling to userpage

--- src/Containers/userPage.js
+++ src/Containers/userPage.js
@@ -13,26 +13,38 @@ const UserPage = () => {
   console.log(userData.appointments);
 
   return (
-    <div>
-      <p>{userData.username}</p>
+    <div className="col-10 m-4">
+      <div className="d-flex">
+        <p className="mr-3  col-2 font-weight-bold">Username:</p>
+        <p>{userData.username}</p>
+      </div>
       { userData.appointments.reserved_service.map((appointments) => (
-        <div key={appointments.id}>
-          <div>
-            Service Requested:
-            {appointments.petService}
+        <div key={appointments.id} className="m-4">
+          <div className="d-flex">
+            <p className="mr-3 col-2 font-weight-bold">Service Requested:</p>
+            <p>
+              {appointments.petService}
+            </p>
+          </div>
+          <div className="d-flex">
+            <p className="mr-3 col-2 font-weight-bold">Service Description:</p>
             <p className="d-flex">
               {appointments.serviceDescription}
             </p>
+          </div>
+          <div className="d-flex">
+            <p className="mr-3 col-2 font-weight-bold">Service Price:</p>
             <p>{appointments.servicePrice}</p>
           </div>
-          <p>
-            Appointments Date:
-            {appointments.date}
-          </p>
-          <p>
-            Appointments Was Created At :
-            {appointments.created_at}
-          </p>
+          <div className="d-flex">
+            <p className="mr-3 col-2 font-weight-bold">Appointments Date:</p>
+            <p>{appointments.date}</p>
+          </div>
+          <div className="d-flex">
+            <p className="mr-3 col-2 font-weight-bold">Appointments Was Created At :</p>
+            <p>{appointments.created_at}</p>
+          </div>
+          <hr className=" col-12 tm-4 tb-4" />
         </div>
       ))}
     </div>
