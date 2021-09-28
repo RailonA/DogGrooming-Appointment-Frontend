5ca546b11be4466600424e644a68ac05f6d2be5f
RailonA <railonacosta@gmail.com>
2021-09-28 13:07:38 -0400

    Show loading message before data fetch for UserData

--- src/Containers/userPage.js
+++ src/Containers/userPage.js
@@ -14,41 +14,50 @@ const UserPage = () => {
   console.log(userData);
 
   return (
-
-    <div className="col-10 m-4">
-      <div className="d-flex">
-        <p className="mr-3  col-2 font-weight-bold">Username:</p>
-        <p>{userData.username}</p>
-      </div>
-      { userData.appointments.appointments.map((appointments) => (
-        <div className="col-10" key={appointments.id}>
-          <div className="d-flex col-12">
-            <p className="mr-3 col-3 font-weight-bold">Service Requested:</p>
-            <p>
-              {appointments.service.petService}
-            </p>
-          </div>
-          <div className="d-flex col-12">
-            <p className="mr-3 col-3 font-weight-bold">Service Description:</p>
-            <p className="d-flex">
-              {appointments.service.serviceDescription}
-            </p>
-          </div>
-          <div className="d-flex col-12">
-            <p className="mr-3 col-3 font-weight-bold">Service Price:</p>
-            <p>{appointments.service.servicePrice}</p>
-          </div>
-          <div className="d-flex col-12">
-            <p className="mr-3 col-3 font-weight-bold">Appointments Date:</p>
-            <p>{appointments.date}</p>
-          </div>
-          <div className="d-flex col-12">
-            <p className="mr-3 col-3 font-weight-bold">Appointments Was Created At :</p>
-            <p>{appointments.created_at}</p>
-          </div>
-          <hr className=" col-12 tm-4 tb-4 mainHr" />
-        </div>
-      ))}
+    <div>
+      {
+        userData.loading
+          ? <span>Loading ...</span>
+          : (
+            <div className="col-10 m-4 d-flex justify-content-center">
+              <div>
+                <div className="d-flex">
+                  <p className="mr-3  col-2 font-weight-bold">Username:</p>
+                  <p>{userData.username}</p>
+                </div>
+                { userData.appointments.appointments.map((appointments) => (
+                  <div className="col-10" key={appointments.id}>
+                    <div className="d-flex col-12">
+                      <p className="mr-3 col-3 font-weight-bold">Service Requested:</p>
+                      <p>
+                        {appointments.service.petService}
+                      </p>
+                    </div>
+                    <div className="d-flex col-12">
+                      <p className="mr-3 col-3 font-weight-bold">Service Description:</p>
+                      <p className="d-flex">
+                        {appointments.service.serviceDescription}
+                      </p>
+                    </div>
+                    <div className="d-flex col-12">
+                      <p className="mr-3 col-3 font-weight-bold">Service Price:</p>
+                      <p>{appointments.service.servicePrice}</p>
+                    </div>
+                    <div className="d-flex col-12">
+                      <p className="mr-3 col-3 font-weight-bold">Appointments Date:</p>
+                      <p>{appointments.date}</p>
+                    </div>
+                    <div className="d-flex col-12">
+                      <p className="mr-3 col-3 font-weight-bold">Appointments Was Created At :</p>
+                      <p>{appointments.created_at}</p>
+                    </div>
+                    <hr className=" col-12 tm-4 tb-4 mainHr" />
+                  </div>
+                ))}
+              </div>
+            </div>
+          )
+      }
     </div>
   );
 };
