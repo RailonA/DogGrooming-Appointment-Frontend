3ec369cfe9965c9f2c0cc31b665cb7782b1e442d
RailonA <railonacosta@gmail.com>
2021-10-26 10:39:09 -0400

    Add TImePicker to select time separate from date

--- src/Components/appointmentform.js
+++ src/Components/appointmentform.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
-import { DatePicker } from 'antd';
+import { DatePicker, TimePicker } from 'antd';
 import { Button, Modal } from 'react-bootstrap';
 import { requestAppointment } from '../Helpers/requests';
 import '../Assets/styles/navBar.css';
@@ -31,7 +31,6 @@ const AppointmentForm = ({
   const handleServiceChange = (e) => {
     if (e.target.name === 'service-selection') {
       serviceId = e.target.value;
-      console.log(serviceId);
       setChooseService(serviceId);
     }
   };
@@ -39,7 +38,6 @@ const AppointmentForm = ({
   const handleDatePickerChange = (e) => {
     date = e;
     setApointment(date);
-    console.log(date);
   };
 
   const handleSubmit = (e) => {
@@ -52,6 +50,10 @@ const AppointmentForm = ({
   const disabledDates = (current) => (
     current < moment().startOf('day') || moment(current).day() === 0 || moment(current).day() === 6
   );
+
+  function onTimeChange(time, timeString) {
+    console.log(time, timeString);
+  }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -66,14 +68,18 @@ const AppointmentForm = ({
           </select>
         </div>
         <DatePicker
-          format="YYYY-MM-DD HH"
+          format="YYYY-MM-DD"
           disabledDate={disabledDates}
+          onChange={(e) => handleDatePickerChange(e)}
+        />
+        <TimePicker
+          onChange={(e) => onTimeChange(e)}
+          format="HH"
           showTime={{
             use12Hours: true,
             defaultValue: moment('00:00:00', 'HH:mm:ss'),
           }}
           disabledHours={() => [0, 1, 2, 3, 4, 5, 6, 7, 19, 20, 21, 22, 23, 24]}
-          onChange={(e) => handleDatePickerChange(e)}
         />
       </Modal.Body>
       <Modal.Footer>
